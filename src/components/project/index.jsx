b77879b02c293ffaf9b5c1a5b0ffc7f73e05b3b0
Amirxon <amirxon@MacBook-Air-alisher.local>
2025-05-24 20:23:57 +0500

    any

--- src/components/project/index.jsx
+++ src/components/project/index.jsx
@@ -1,28 +1,39 @@
-import { Link } from "react-router-dom";
-import {RevalAboutContent} from '../../ui/introImg'
-import {projectCard} from '../const';
+import { RevalAboutContent } from '../../ui/introImg'
+import { projectCard } from '../const';
 import "./_style.scss"
-const index = () => {
+
+const Index = () => {
+
+  const handleClick = (e, projectId) => {
+    e.preventDefault(); 
+    window.location.href = `/project/${projectId}`;  
+  };
+
   return (
     <div>
       <div className="container">
         <div id="projects">
           <RevalAboutContent>
-          <h2>Projects</h2>
-          <h3>Some of my latest projects and work.</h3>
+            <h2>Projects</h2>
+            <h3>Some of my latest projects and work.</h3>
           </RevalAboutContent>
           <div className="gridItem">
-          {
-            projectCard.map((project) => (
-              <Link to={`/project/${project.id}`} key={project.id} className="gridCard">
-                <div className="gridImg">
-                  <img src={project.image} />
-                </div>
-                <h3>{project.name}</h3>
-                <i className="bi bi-eye-fill"></i>
-              </Link>
-            ))
-          }
+            {
+              projectCard.map((project) => (
+                <a
+          href={`/project/${project.id}`}
+          key={project.id}
+          className="gridCard"
+          onClick={(e) => handleClick(e, project.id)}
+        >
+          <div className="gridImg">
+            <img src={project.image} alt={project.name} />
+          </div>
+          <h3>{project.name}</h3>
+          <i className="bi bi-eye-fill"></i>
+        </a>
+              ))
+            }
           </div>
         </div>
       </div>
@@ -30,4 +41,4 @@ const index = () => {
   )
 }
 
-export default index
+export default Index;
--- src/ui/single/index.jsx
+++ src/ui/single/index.jsx
@@ -1,34 +1,43 @@
+// ui/single/ProjectItem.jsx
 import { useParams, useNavigate } from "react-router-dom";
-import {RevalTopBottom} from '../../ui/introImg'
+import { RevalTopBottom } from "../../ui/introImg";
 import { projectCard } from "../../components/const";
 import "./_style.scss";
+
 export const ProjectItem = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const project = projectCard.find((item) => item.id === parseInt(id));
+
+  const project = projectCard.find((item) => item.id === Number(id));
+
   if (!project) {
     return <h2>Project topilmadi!</h2>;
   }
+
   return (
     <div className="single">
       <div className="top">
-          <h1>{project.name}</h1>
-          <button onClick={()=>navigate(-1)}>Back</button>
+        <h1>{project.name}</h1>
+        <button onClick={() => navigate(-1)}>Back</button>
       </div>
       <div className="page">
         <div className="container">
           <div className="wrapper">
-              <img src={project.image} alt="" />
-              <div className="right">
-            <RevalTopBottom>
-              <h3>Used Stack:</h3>
-              <h4>{project.techStack.join(",  ")}  </h4>
-              <p>{project.description}</p>
-              <div className="links">
-              <a href={project.link}> <i className="bi bi-link-45deg"></i>live</a>
-              <a href={project.github}>GitHub</a>
-            </div>
-            </RevalTopBottom>
+            <img src={project.image} alt={project.name} />
+            <div className="right">
+              <RevalTopBottom>
+                <h3>Used Stack:</h3>
+                <h4>{project.techStack.join(", ")}</h4>
+                <p>{project.description}</p>
+                <div className="links">
+                  <a href={project.link} target="_blank" rel="noopener noreferrer">
+                    <i className="bi bi-link-45deg"></i> Live
+                  </a>
+                  <a href={project.github} target="_blank" rel="noopener noreferrer">
+                    GitHub
+                  </a>
+                </div>
+              </RevalTopBottom>
             </div>
           </div>
         </div>
